a8b27f5d47458f213c99b64625e320085c4e2cb1
GadongKarlSaid <146708073+GadongKarlSaid@users.noreply.github.com>
2023-10-04 14:47:37 +0800

    feat: Activity 1 in DSA

---
+++ script.js
@@ -0,0 +1,12 @@
+//Activity 1 
+
+let firstFavNum = 14;
+let secondFavNum = 100;
+
+console.log(firstFavNum + secondFavNum);
+console.log(firstFavNum - secondFavNum);
+console.log(firstFavNum * secondFavNum);
+console.log(firstFavNum / secondFavNum);
+console.log(firstFavNum % secondFavNum);
+console.log(Math.pow(firstFavNum));
+
